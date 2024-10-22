d761f6f567973db98b0bb5cc1bfcb5850baedd9d
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-10-22 09:41:22 +0700

    Commit

---
+++ database/migrations/2024_10_22_020833_update_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (Schema::hasTable('users') && !Schema::hasColumn('users', 'idRole')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->unsignedBigInteger('idRole')->after('email')->nullable();
+                $table->boolean('status')->default(1);
+                $table->foreign('idRole')->references('id')->on('roles');
+            });
+        } 
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
