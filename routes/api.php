e8f5bf502328a49981619c04b621ff354585f29e
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-10-24 16:44:42 +0700

    Commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FeaturesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/features',[FeaturesController::class,'api_index']);
+Route::get('/features/{id}',[FeaturesController::class,'api_detail']);
