67a45fb38c01565c36f1926cee0bc51c88a3ef49
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-11-05 13:47:01 +0700

    Commit

--- config/app.php
+++ config/app.php
@@ -27,7 +27,17 @@ return [
     | services the application utilizes. Set this in your ".env" file.
     |
     */
-
+    'image_url'=>'https://toonappbackend.trungthanhzone.com/storage/',
+     /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services the application utilizes. Set this in your ".env" file.
+    |
+    */
     'env' => env('APP_ENV', 'production'),
 
     /*
