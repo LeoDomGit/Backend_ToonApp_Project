6a8987d51a00a0d79817ad4f601c3b548dbb936d
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-11-08 10:46:18 +0700

    Commit

--- database/migrations/2024_11_07_145901_create_keys_table.php
+++ database/migrations/2024_11_07_145901_create_keys_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('keys', function (Blueprint $table) {
             $table->id();
             $table->string('api',255);
+            $table->string('key',255);
             $table->timestamps();
         });
     }
