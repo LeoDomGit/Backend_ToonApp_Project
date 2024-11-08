363d4d1e4d17f20ddf6695eb7e75a2f0209c1f5c
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-11-08 14:40:35 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -33,16 +33,17 @@ Route::middleware('device_login')->group(function () {
     Route::put('/customers', [CustomersController::class, 'update']);
     Route::get('/logout',[CustomersController::class, 'logout']);
     Route::post('/upload_image',[ImageAIController::class,'uploadImage']);
-    Route::prefix('image_ai')->group(function () {
-        Route::post('/style',[ImageAIController::class,'cartoon']);
-        // Route::post('/remove_background',[ImageAIController::class,'changeBackground']);
-        // Route::post('/claymation',[ImageAIController::class,'claymation']);
-        // Route::post('/disney_charactors',[ImageAIController::class,'disneyCharators']);
-        // Route::post('/fullbody_cartoon',[ImageAIController::class,'fullBodyCartoon']);
-        // Route::post('/animal_toon',[ImageAIController::class,'animalToon']);
-        // Route::post('/new_profile_pic',[ImageAIController::class,'newProfilePic']);
-        // Route::post('/funny_charactors',[ImageAIController::class,'funnyCharactors']);
-    });
+    Route::post('/style',[ImageAIController::class,'cartoon']);
+    // Route::prefix('image_ai')->group(function () {
+        
+    //     // Route::post('/remove_background',[ImageAIController::class,'changeBackground']);
+    //     // Route::post('/claymation',[ImageAIController::class,'claymation']);
+    //     // Route::post('/disney_charactors',[ImageAIController::class,'disneyCharators']);
+    //     // Route::post('/fullbody_cartoon',[ImageAIController::class,'fullBodyCartoon']);
+    //     // Route::post('/animal_toon',[ImageAIController::class,'animalToon']);
+    //     // Route::post('/new_profile_pic',[ImageAIController::class,'newProfilePic']);
+    //     // Route::post('/funny_charactors',[ImageAIController::class,'funnyCharactors']);
+    // });
 });
 Route::get('/backgrounds',[BackgroundController::class,'api_index']);
 
