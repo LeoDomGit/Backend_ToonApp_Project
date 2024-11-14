5dd49069514cd18023cb83af45f21d9a6f70e78f
Leo Git <leodomsolar@gmail.com>
2024-11-14 09:12:58 +0700

    Update FeaturesController.php

--- app/Http/Controllers/FeaturesController.php
+++ app/Http/Controllers/FeaturesController.php
@@ -103,7 +103,7 @@ class FeaturesController
         }
         $arr=$request->size_id;
         FeaturesSizes::where('feature_id',$id)->delete();
-        if(count($arr)!=0){
+        if($arr==null){
             foreach ($arr as $key => $value) {
                 FeaturesSizes::create([
                  'feature_id'=>$id,
