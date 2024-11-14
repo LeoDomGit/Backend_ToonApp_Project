edd630b2586b5d2b00aecc22c30397a54f21ccba
Leo Git <leodomsolar@gmail.com>
2024-11-14 10:34:40 +0700

    Update Features.php

--- app/Models/Features.php
+++ app/Models/Features.php
@@ -46,13 +46,8 @@ class Features extends Model
             return $attributes['image'] ? asset('storage/' . $attributes['image']) : null;
         });
     }
-    public function sizes()
-    {
-        return $this->belongsToMany(ImageSize::class, 'features_sizes', 'feature_id', 'size_id')
-                    ->withPivot(['model_id', 'prompt', 'presetStyle', 'preprocessorId', 'strengthType', 'initImageId'])
-                    ->as('features_sizes')
-                    ->wherePivot('model_id', '!=', null) // Optional: only if you want to retrieve with filters
-                    ->select('id', 'size'); // Only include selected fields in query to hide
+    public function sizes() {
+        return $this->belongsToMany(ImageSize::class, 'features_sizes', 'feature_id', 'size_id');
     }
     public function scopeActive($query)
     {
