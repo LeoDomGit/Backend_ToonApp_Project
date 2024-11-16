810acdb78c4e743b1f6caf1063b783d42aa43101
Leo Git <leodomsolar@gmail.com>
2024-11-16 08:36:33 +0700

    Update Index.jsx

--- resources/js/Pages/Features/Index.jsx
+++ resources/js/Pages/Features/Index.jsx
@@ -895,7 +895,7 @@ function Index({ datafeatures, datasizes }) {
                                         <Box
                                             sx={{
                                                 width: "100%",
-                                                height: "30vh",
+                                                height: "50vh",
                                                 overflowX: "auto",
                                                 overflowY: "hidden",
                                             }}
